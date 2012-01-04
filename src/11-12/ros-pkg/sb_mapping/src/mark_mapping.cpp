209f8fd1cdc319e9b9dc8f19a32c0ec9e241e5bc
Mark <mark@ubuntu.(none)>
2012-01-04 12:41:02 -0800

    added src file

---
+++ src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
@@ -0,0 +1,28 @@
+/**
+
+ros mapping
+
+using gmapping node
+
+version 0.1
+
+testing the communication between
+
+gmapping via map_server
+
+
+*/
+
+
+#include <ros/ros.h>
+#include <std_msgs.h>
+
+using namespace ros;
+using 
+
+int main (int argc, char** argv)
+{
+
+
+
+}
