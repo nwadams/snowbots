621d0e33f0997c224fbf205087282b083fcd9721
Mark <mark@ubuntu.(none)>
2012-01-05 13:08:12 -0800

    made the mapping node listen to gmapping node

--- src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
+++ src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
@@ -15,14 +15,35 @@ gmapping via map_server
 
 
 #include <ros/ros.h>
-#include <std_msgs.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <string>
 
 using namespace ros;
-using 
+using namespace std;
+
+const static string NODE_NAME = "map_listener";
+const static string ROS_SUBSCRIBE_TOPIC = "map";
+const static int LOOP_FREQ = 30;
+
+void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+	ROS_INFO("I have got the map");
+}
 
 int main (int argc, char** argv)
 {
+	init(argc,argv,NODE_NAME);
 
+	NodeHandle n;
+	Subscriber sub = n.subscribe(ROS_SUBSCRIBE_TOPIC, 1000, mapCallback);
+
+	ros::Rate loop_rate(LOOP_FREQ);
+
+	while(ros::ok())
+	{
+		spin();
+
+	}
 
 
 }
