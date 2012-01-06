90d0056826793a6780f716f1c77eb779478c89d8
Mark <mark@ubuntu.(none)>
2012-01-05 18:19:33 -0800

    version 0.3 tried to get map data..and number = 16M

--- src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
+++ src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
@@ -4,7 +4,7 @@ ros mapping
 
 using gmapping node
 
-version 0.1
+version 0.3
 
 testing the communication between
 
@@ -16,7 +16,10 @@ gmapping via map_server
 
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <std_msgs/Float64.h>
 #include <string>
+#include <iostream>
+#include <stdlib.h>
 
 using namespace ros;
 using namespace std;
@@ -27,7 +30,23 @@ const static int LOOP_FREQ = 30;
 
 void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
+	double data_num = msg->data.size();
 	ROS_INFO("I have got the map");
+	cout << "msg data size is " << data_num << endl;
+	/*
+	for(int i = 0; i < data_num; i++)
+	{
+		cout << "The map data " << msg->data[i] << endl;
+	}
+*/
+}
+
+void mapCallback2(const std_msgs::Float64::ConstPtr& msg)
+{
+	double data = msg->data;
+	ROS_INFO("I have recieved entropy");
+	cout << "entropy data is " << data << endl;
+
 }
 
 int main (int argc, char** argv)
@@ -36,13 +55,13 @@ int main (int argc, char** argv)
 
 	NodeHandle n;
 	Subscriber sub = n.subscribe(ROS_SUBSCRIBE_TOPIC, 1000, mapCallback);
+	Subscriber sub1 = n.subscribe("slam_gmapping/entropy", 1000, mapCallback2);
 
-	ros::Rate loop_rate(LOOP_FREQ);
+	//ros::Rate loop_rate(LOOP_FREQ);
 
 	while(ros::ok())
 	{
 		spin();
-
 	}
 
 
