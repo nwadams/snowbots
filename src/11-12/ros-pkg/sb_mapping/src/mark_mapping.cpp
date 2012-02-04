36a4617e8577bac28aed2473194b12a43684beb3
Mark <mark@ubuntu.(none)>
2012-02-04 10:30:04 -0800

    map can tell me where the robot is now

--- src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
+++ src/11-12/ros-pkg/sb_mapping/src/mark_mapping.cpp
@@ -32,10 +32,15 @@ const static string NODE_NAME = "map_listener";
 const static string ROS_SUBSCRIBE_TOPIC = "map";
 const static int LOOP_FREQ = 30;
 
+int robot_x = 0;
+int robot_y = 0;
+
+
 void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
 	double data_num = msg->data.size();
 	long count = 0;
+
 	ROS_INFO("I have got the map");
 	cout << "msg data size is " << data_num << endl;
 	for(long i = 0; i < data_num; i++)
@@ -45,27 +50,8 @@ void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 		//cout << "The map data " << msg->data[i] << endl;
 	}
 	cout << "the number of known data is " << count << endl;
-	double robot_x,robot_y,robot_z;
-	double ori_x, ori_y, ori_z, ori_w;
-	robot_x = msg->info.origin.position.x;
-	robot_y = msg->info.origin.position.y;
-	robot_z = msg->info.origin.position.z;
-	ori_x = msg->info.origin.orientation.x;
-	ori_y = msg->info.origin.orientation.y;
-	ori_z = msg->info.origin.orientation.z;
-	ori_w = msg->info.origin.orientation.w;
-	cout << "the map origin is " << robot_x << ", " << robot_y
-		<< ", " << robot_z << endl;
 
-	cout << "the map orientation is " << ori_x << ", " << ori_y
-		<< ", " << ori_z << ", " << ori_w << endl;
-}
 
-void mapCallback2(const std_msgs::Float64::ConstPtr& msg)
-{
-	double data = msg->data;
-	ROS_INFO("I have recieved entropy");
-	cout << "entropy data is " << data << endl;
 }
 
 int main (int argc, char** argv)
@@ -73,13 +59,13 @@ int main (int argc, char** argv)
 	init(argc,argv,NODE_NAME);
 
 	NodeHandle n;
-	//Subscriber sub = n.subscribe(ROS_SUBSCRIBE_TOPIC, 1000, mapCallback);
+	Subscriber sub = n.subscribe(ROS_SUBSCRIBE_TOPIC, 1000, mapCallback);
 	//Subscriber sub1 = n.subscribe("slam_gmapping/entropy", 1000, mapCallback2);
 
 	
 	Rate loop_rate(LOOP_FREQ);
-
 	TransformListener listener;
+
 
 	while(ros::ok())
 	{
@@ -92,8 +78,10 @@ int main (int argc, char** argv)
 		{
 			ROS_ERROR("%s", ex.what());
 		}
+		robot_x = transform.getOrigin().x();
+		robot_y = transform.getOrigin().y();
 		cout << "the robot is " << transform.getOrigin().x() << ", "
-			<< transform.getOrigin().y() << endl;
+				<< transform.getOrigin().y() << "when new map data was fed in" << endl;
 		loop_rate.sleep();
 	}
 
